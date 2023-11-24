f202b641075f48669cf3ed984f7f7b6d0c264b90
Muhammad Doukmak <mkdoukmak@gmail.com>
2023-11-24 12:05:15 -0600

    Add new data and response to stub

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -29,7 +29,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
         URLProtocolStub.startInterceptingRequests()
         let url = URL(string: "https://any-url")!
         let error = NSError(domain: "Test", code: 0)
-        URLProtocolStub.stub(url: url, error: error)
+        URLProtocolStub.stub(url: url, data: nil, response: nil, error: error)
 
         let sut = URLSessionHTTPClient()
         let exp = expectation(description: "wait")
@@ -52,11 +52,13 @@ final class URLSessionHTTPClientTests: XCTestCase {
         private static var stubs = [URL: Stub]()
 
         private struct Stub {
+            let data: Data?
+            let response: URLResponse?
             let error: Error?
         }
 
-        static func stub(url: URL, error: Error? = nil) {
-            stubs[url] = Stub(error: error)
+        static func stub(url: URL, data: Data?, response: URLResponse?, error: Error?) {
+            stubs[url] = Stub(data: data, response: response, error: error)
         }
 
         static func startInterceptingRequests() {
@@ -81,6 +83,14 @@ final class URLSessionHTTPClientTests: XCTestCase {
             guard let url = request.url,
                   let stub = URLProtocolStub.stubs[url] else { return }
             
+            if let data = stub.data {
+                client?.urlProtocol(self, didLoad: data)
+            }
+
+            if let response = stub.response {
+                client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            }
+
             if let error = stub.error {
                 client?.urlProtocol(self, didFailWithError: error)
             }
@@ -89,19 +99,5 @@ final class URLSessionHTTPClientTests: XCTestCase {
         }
 
         override func stopLoading() { }
-    }
-
-    private class FakeURLSessionDataTask: URLSessionDataTask {
-        override func resume() {
-
-        }
-    }
-
-    private class URLSessionDataTaskSpy: URLSessionDataTask {
-        var resumeCallCount = 0
-
-        override func resume() {
-            resumeCallCount += 1
-        }
     }
 }
