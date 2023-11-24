846acb734eb9a40e6ef7eccd2e666c048575a5fa
Muhammad Doukmak <mkdoukmak@gmail.com>
2023-11-24 08:01:36 -0600

    Create FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Muhammad Doukmak on 11/24/23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
