94f0bc709ac4cbae71c558220326659a649efd3a
Muhammad Doukmak <mkdoukmak@gmail.com>
2023-11-24 22:37:23 -0600

    load delivers no images on more than 7 days old cache

--- EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -51,7 +51,7 @@ final class LoadFeedFromCacheUseCaseTests: XCTestCase {
         }
     }
 
-    func test_load_deliversCachedFeedOnSevenDaysOldCache() {
+    func test_load_deliversEmptyFeedOnSevenDaysOldCache() {
         let feed = uniqueImageFeed()
         let fixedCurrentDate = Date()
         let sevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7)
@@ -61,6 +61,18 @@ final class LoadFeedFromCacheUseCaseTests: XCTestCase {
             store.completeRetrievalWith(with: feed.local, timestamp: sevenDaysOldTimestamp)
         }
     }
+
+    func test_load_deliversEmptyFeedOnMoreThanSevenDaysOldCache() {
+        let feed = uniqueImageFeed()
+        let fixedCurrentDate = Date()
+        let moreThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: -1)
+        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+
+        expect(sut, toCompleteWith: .success([])) {
+            store.completeRetrievalWith(with: feed.local, timestamp: moreThanSevenDaysOldTimestamp)
+        }
+    }
+
 
     // MARK: - Helpers
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
