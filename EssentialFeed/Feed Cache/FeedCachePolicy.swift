90a5e34ce52ce5ebcb07d1206151485256a3c9e3
Muhammad Doukmak <mkdoukmak@gmail.com>
2023-11-24 23:27:11 -0600

    Move FeedCachePolicy to its own file

---
+++ EssentialFeed/Feed Cache/FeedCachePolicy.swift
@@ -0,0 +1,22 @@
+//
+//  FeedCachePolicy.swift
+//  EssentialFeed
+//
+//  Created by Muhammad Doukmak on 11/24/23.
+//
+
+import Foundation
+
+final class FeedCachePolicy {
+    private static let calendar = Calendar(identifier: .gregorian)
+    private static var maxCacheAgeInDays = 7
+
+    private init() {}
+
+    static func validate(_ timestamp: Date, against date: Date) -> Bool {
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else {
+            return false
+        }
+        return date < maxCacheAge
+    }
+}
