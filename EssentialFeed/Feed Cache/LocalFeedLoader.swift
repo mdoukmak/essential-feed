4978826502568c8c43687b0cc4e6ca61cb05ce13
Muhammad Doukmak <mkdoukmak@gmail.com>
2023-11-24 23:10:19 -0600

    Extract cache deleiton side effects on expired cache from the load to the validate Cache

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -38,7 +38,6 @@ public final class LocalFeedLoader {
             case let .found(feed, timestamp) where self.validate(timestamp):
                 completion(.success(feed.toModels()))
             case .found:
-                store.deleteCachedFeed { _ in }
                 completion(.success([]))
             case .empty:
                 completion(.success([]))
@@ -51,9 +50,11 @@ public final class LocalFeedLoader {
     public func validateCache() {
         store.retrieve { [unowned self] result in
             switch result {
+            case let .found(feed: _, timestamp: timestamp) where !validate(timestamp):
+                self.store.deleteCachedFeed { _ in }
             case .failure:
                 self.store.deleteCachedFeed { _ in }
-            default: break
+            case .empty, .found: break
             }
         }
     }
