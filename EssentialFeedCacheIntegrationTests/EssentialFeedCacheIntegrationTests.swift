49e8e52949c804294162736cbc77682bbbde78b4
Muhammad Doukmak <mkdoukmak@gmail.com>
2023-11-25 10:44:25 -0600

    Add EssentialFeedCacheIntegrationTests target

---
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,14 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Muhammad Doukmak on 11/25/23.
+//
+
+import XCTest
+
+final class EssentialFeedCacheIntegrationTests: XCTestCase {
+    func test_() {
+
+    }
+}
