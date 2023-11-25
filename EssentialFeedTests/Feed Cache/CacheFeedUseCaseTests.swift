0331d2c77d544a150f8243df02f61c9136bac3ad
Muhammad Doukmak <mkdoukmak@gmail.com>
2023-11-24 18:14:41 -0600

    save requests cache deletion

--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -6,15 +6,25 @@
 //
 
 import XCTest
+import EssentialFeed
 
 final class LocalFeedLoader {
+    private let store: FeedStore
     init(store: FeedStore) {
+        self.store = store
+    }
 
+    func save(_ items: [FeedItem]) {
+        store.deleteCachedFeed()
     }
 }
 
 final class FeedStore {
     var deleteCachedFeedCallCount = 0
+
+    func deleteCachedFeed() {
+        deleteCachedFeedCallCount += 1
+    }
 }
 
 final class CacheFeedUseCaseTests: XCTestCase {
@@ -22,6 +32,25 @@ final class CacheFeedUseCaseTests: XCTestCase {
         let store = FeedStore()
         _ = LocalFeedLoader(store: store)
 
+
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+    }
+
+    func test_save_requestsCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        let items = [uniqueItem(), uniqueItem()]
+
+        sut.save(items)
+
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+    }
+
+    private func uniqueItem() -> FeedItem {
+        return FeedItem(id: UUID(), description: "any", location: "any", imageURL: anyURL())
+    }
+    
+    private func anyURL() -> URL {
+        URL(string: "https://any-url")!
     }
 }
