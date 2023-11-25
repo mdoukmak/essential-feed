24750394a6e54e3f9907e816248ef86567e78e6e
Muhammad Doukmak <mkdoukmak@gmail.com>
2023-11-25 08:57:43 -0600

    Track for memory leaks

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -126,7 +126,9 @@ final class CodableFeedStoreTests: XCTestCase {
     }
 
     // MARK: - Helpers
-    private func makeSUT() -> CodableFeedStore {
-        CodableFeedStore()
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> CodableFeedStore {
+        let sut = CodableFeedStore()
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
     }
 }
