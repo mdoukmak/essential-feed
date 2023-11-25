ab777822613bf8c415cd38fd18335d230ff1c229
Muhammad Doukmak <mkdoukmak@gmail.com>
2023-11-24 20:06:22 -0600

    Move LocalFeedItem to its own file

---
+++ EssentialFeed/Feed Cache/LocalFeeditem.swift
@@ -0,0 +1,22 @@
+//
+//  LocalFeeditem.swift
+//  EssentialFeed
+//
+//  Created by Muhammad Doukmak on 11/24/23.
+//
+
+import Foundation
+
+public struct LocalFeedItem: Equatable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+
+    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+}
