8ca4efbf376aa5d9f265cdd7333dd4a277c4f578
Muhammad Doukmak <mkdoukmak@gmail.com>
2023-11-25 22:58:29 -0600

    retry button retires image load

--- EssentialFeediOS/FeedViewController.swift
+++ EssentialFeediOS/FeedViewController.swift
@@ -82,6 +82,15 @@ public final class FeedViewController: UITableViewController {
             cell?.feedImageRetryButton.isHidden = (image != nil)
             cell?.feedImageContainer.stopShimmering()
         }
+        cell.onRetry = { [weak self] in
+            self?.tasks[indexPath] = self?.imageLoader?.loadImageData(from: cellModel.url) { [weak cell] result in
+                let data = try? result.get()
+                let image = data.map(UIImage.init) ?? nil
+                cell?.feedImageView.image = image
+                cell?.feedImageRetryButton.isHidden = (image != nil)
+                cell?.feedImageContainer.stopShimmering()
+            }
+        }
         return cell
     }
 
